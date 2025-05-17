267f468d22b4fc8babccce5cc0099eac79de69f8
liyang <liyang@whlhkj.cn>
2025-05-17 21:27:37 +0800

    默认展开

--- src/lib/components/common/Collapsible.svelte
+++ src/lib/components/common/Collapsible.svelte
@@ -36,7 +36,7 @@
 	import Markdown from '../chat/Messages/Markdown.svelte';
 	import Image from './Image.svelte';
 
-	export let open = false;
+	export let open = true;
 
 	export let className = '';
 	export let buttonClassName =
